3d79b8f4dc9ef8c16b755968434d59981134da48
jayawickrama <nishantharaka2004@gmail.com>
2025-04-04 09:14:56 +0530

    created dto class

--- src/main/java/com/example/senturainterviewonline/DTO/UserDTO.java
+++ src/main/java/com/example/senturainterviewonline/DTO/UserDTO.java
@@ -1,4 +1,5 @@
 package com.example.senturainterviewonline.DTO;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,17 +13,14 @@ public class UserDTO {
     @Pattern(regexp = ".*\\D.*", message = "UID must contain at least one non-digit character")
     private String uid;
 
-    @Email(message = "Invalid email format")
-    private String email;
-
-    @Size(max = 50, message = "Given name cannot be more than 50 characters")
-    private String givenName;
-
-    @Size(max = 50, message = "Middle name cannot be more than 50 characters")
-    private String middleName;
-
     @Size(max = 100, message = "Name cannot be more than 100 characters")
     private String name;
+
+    @Size(max = 50, message = "Given name cannot be more than 50 characters")
+    private String given_name;
+
+    @Size(max = 50, message = "Middle name cannot be more than 50 characters")
+    private String middle_name;
 
     @Size(max = 50, message = "Family name cannot be more than 50 characters")
     private String familyName;
@@ -30,8 +28,11 @@ public class UserDTO {
     @Size(max = 50, message = "Nickname cannot be more than 50 characters")
     private String nickname;
 
+    @Email(message = "Invalid email format")
+    private String email;
+
     @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Invalid phone number format")
-    private String phoneNumber;
+    private String phone_number;
 
     @Size(max = 255, message = "Comment cannot be more than 255 characters")
     private String comment;
@@ -44,10 +45,4 @@ public class UserDTO {
     private Map<String, Object> metadata;
 
     private List<String> tags;
-
-    @NotNull(message = "Suspension status must be provided")
-    private Boolean isSuspended;
-
-    @NotNull(message = "Bot status must be provided")
-    private Boolean isBot;
 }
