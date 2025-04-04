e3f40610a4c6698e844bc148e868694afcfe090a
jayawickrama <nishantharaka2004@gmail.com>
2025-04-04 08:16:33 +0530

    first commit

---
+++ src/main/java/com/example/senturainterviewonline/SenturaInterviewOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.senturainterviewonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaInterviewOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenturaInterviewOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/senturainterviewonline/SenturaInterviewOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.senturainterviewonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenturaInterviewOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
